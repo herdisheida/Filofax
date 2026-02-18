85b5c7226b505f405730e2007f4edfe8406664f6
Herdis Heida <herdisj24@ru.is>
2026-02-18 15:16:36 +0000

    fix models
    extenda contact (individual type og company type )

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
-interface Contact<contactType> {
+import prepopulation from "./data/prepopulation.json";  // get data
+
+/* 
+  types and models
+*/
+
+enum ContactType {
+  Individual = "individual",
+  Company = "company"
+}
+type Contact = IndividualContact | CompanyContact;
+
+interface BaseContact {
+  id: string;
   name: string;
   thumbnail: string;
-  type: contactType;
-  info: IndividualInfo | CompanyInfo;
+  type: ContactType;
+  isExpanded?: boolean;  // card expansion state
 }
 
 interface IndividualInfo {
@@ -13,16 +26,26 @@ interface IndividualInfo {
   website: string;
 }
 
+interface CompanyKeyContact {
+  name: string;
+  email_address: string;
+}
+
 interface CompanyInfo {
   phone_number: string;
   industry: string;
   email_address: string;
   address: string;
   website: string;
-  key_contacts: IndividualInfo[];
+  key_contacts: CompanyKeyContact[];
 }
 
-enum ContactType {
-  Individual, // 0
-  Company, // 1
+interface IndividualContact extends BaseContact {
+  type: ContactType.Individual;
+  info: IndividualInfo;
+}
+
+interface CompanyContact extends BaseContact {
+  type: ContactType.Company;
+  info: CompanyInfo;
 }
